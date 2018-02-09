e679aca888da5a08946dc1daf7deeb6806183775
Brady James Garvin <bgarvin@cse.unl.edu>
2018-02-09 12:40:20 -0600

    Removed compatibility code and added menu options enum.

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,15 @@
-from __future__ import print_function, division
+from enum import Enum
 
-from builtins import input
+
+class MenuOption(Enum):
+    SHOW_REVIEW = 'Show a review'
+    SAVE_TOKEN_LIST = 'Save the token list to a file'
+    SHOW_DOCUMENT_FREQUENCY = 'Show the document frequency for a particular token'
+    SHOW_TOKEN_STATISTICS = 'Show all statistics for a particular token'
+    SHOW_SENTENCE_STATISTICS = 'Show the statistics for a sentence'
+    SAVE_STOP_WORD_LIST = 'Save the list of stop words to a file'
+    SHOW_ADJUSTED_SENTENCE_STATISTICS = 'Show the statistics for a sentence with stop words ignored'
+    EXIT = 'Exit the program'
 
 
 def main():
