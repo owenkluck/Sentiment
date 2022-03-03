1f215440bfb212a321810c994aaa63324ab22381
Owen Kluck <okluck2@huskers.unl.edu>
2022-03-03 12:31:02 -0600

    Sorted text into list and created the user prompt for amount of reviews.

--- sentiment.py
+++ sentiment.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class MenuOption(Enum):
+
     SHOW_REVIEWS = 'Show reviews'
     CHECK_TOKEN = 'Check if a token is present'
     SHOW_DOCUMENT_FREQUENCY = 'Show the document frequency for a particular token'
@@ -13,6 +14,7 @@ class MenuOption(Enum):
 
 
 def option_input(options):
+
     while True:
         try:
             chosen_option = int(input('Enter a number from 1 to 8:'))
@@ -24,6 +26,20 @@ def option_input(options):
 
 
 def main():
+
+    with open("sentiment.txt", "r") as sentiment_text:
+        reviews: []
+        try:
+            for a_line in sentiment_text:
+                strip_lines = a_line.strip()
+                reviews = strip_lines.split()
+                print(reviews)
+                m = reviews.append(reviews)
+                values = a_line.split()
+            print(values)
+        except FileExistsError:
+            print("This file does not exist")
+
     options = tuple(MenuOption)
     while True:
         print('Choose an option:')
@@ -34,6 +50,18 @@ def main():
             return
         else:
             print(f'{input_option}\n')
+
+        value1 = input('Enter a beginning review number from 1 to 8529: ')
+        value2 = input('Enter a ending review number from ' + value1 + ' to 8529: ')
+        converted_value1 = int(value1)
+        converted_value2 = int(value2)
+        if input_option == MenuOption.SHOW_REVIEWS:
+            print('Enter a beginning review number from 1 to 8529: ')
+        if 1 <= converted_value1 <= 8529:
+            print('Enter a ending review number from ' + value1 + ' to 8529: ')
+        if converted_value1 <= converted_value2 <= 8529:
+            print(reviews[converted_value1 - 1:converted_value2 - 1])
+
 
 
 if __name__ == '__main__':
