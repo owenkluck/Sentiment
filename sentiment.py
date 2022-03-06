312063b444a0a5bdea4a18e79eb647d0132f0ef3
dives2 <dives2@huskers.unl.edu>
2022-03-05 20:22:58 -0600

    Refactored main method and implemented show_sentence_statistics method

--- sentiment.py
+++ sentiment.py
@@ -21,11 +21,15 @@ def make_review_list():
     return reviews
 
 
-def make_token_set(reviews):
-    tokens = []
+def make_token_dictionary(reviews):
+    tokens = dict()
     for review in reviews:
-        tokens.extend(review[1:].split())
-    return set(tokens)
+        for token in review[1:].split():
+            if token in tokens:
+                tokens[token] += 1
+            else:
+                tokens[token] = 1
+    return tokens
 
 
 def get_input_number(input_prompt, smallest_value, collection):
@@ -41,50 +45,36 @@ def get_input_number(input_prompt, smallest_value, collection):
             print('Please enter a valid, in-range number')
 
 
-def get_token_frequency(reviews, input_token):
-    number_of_appearances = 0
-    for review in reviews:
-        for token in review[1:].split():
-            if token == input_token:
-                number_of_appearances += 1
-    return number_of_appearances
+def show_reviews(reviews):
+    beginning_review_number = get_input_number(f'Enter a beginning review number from 1 to {len(reviews)}: ', 0,
+                                               reviews)
+    ending_review_number = get_input_number(
+        f'Enter a ending review number from {beginning_review_number} to {len(reviews)}: ',
+        beginning_review_number - 1, reviews)
+    for review in reviews[beginning_review_number - 1:ending_review_number]:
+        print(f'Review #{reviews.index(review) + 1}: {review}')
 
 
-def get_token_statistics(reviews, tokens, input_token):
+def check_token(tokens):
     input_token = input("Enter a token: ").lower()
     if input_token in tokens:
-        review_type_appearances = get_review_type_appearances(input_token, reviews)
-        differential_score = compute_differential_score(review_type_appearances, reviews)
-        differential_classification = compute_differential_classification(differential_score)
-        print(
-            f'The token "{input_token}" has {review_type_appearances["negative"]} negative, {review_type_appearances["neutral"]} neutral, and {review_type_appearances["positive"]} positive appearance(s) in the training data.')
-        print(
-            f'The token "{input_token}" is classified as {differential_classification} because it has has a differential tf-idf score of {differential_score}')
+        print(f'The token "{input_token}" is one of the {len(tokens)} unique tokens that appear in the training data.')
     else:
-        print(f'The token "{input_token}" does not appear in the training data.')
+        print(
+            f'The token "{input_token}" is not one of the {len(tokens)} unique tokens that appear in the training data.')
 
 
-def compute_differential_classification(differential_score):
-    if differential_score < -0.1:
-        return 'negative'
-    elif differential_score > 0.1:
-        return 'positive'
+def get_token_frequency(tokens, input_token):
+    if input_token in tokens:
+        return tokens[input_token]
     else:
-        return 'neutral'
+        return 0
 
 
-def compute_differential_score(review_type_appearances, reviews):
-    all_tokens_in_positives = 0
-    all_tokens_in_negatives = 0
-    for review in reviews:
-        for token in review[1:].split():
-            if review[0] == '+':
-                all_tokens_in_positives += 1
-            if review[0] == '-':
-                all_tokens_in_negatives += 1
-    differential_score = (math.log(1 + review_type_appearances['positive']) - math.log(1 + all_tokens_in_positives)) - (
-            math.log(1 + review_type_appearances['negative']) - math.log(1 + all_tokens_in_negatives))
-    return differential_score
+def show_document_frequency(tokens):
+    input_token = input("Enter a token: ").lower()
+    number_of_appearances = get_token_frequency(tokens, input_token)
+    print(f'The training data contains {number_of_appearances} appearance(s) of the token "{input_token}".')
 
 
 def get_review_type_appearances(input_token, reviews):
@@ -101,13 +91,74 @@ def get_review_type_appearances(input_token, reviews):
     return review_type_appearances
 
 
+def compute_differential_score(review_type_appearances, reviews):
+    all_tokens_in_positives = 0
+    all_tokens_in_negatives = 0
+    for review in reviews:
+        for token in review[1:].split():
+            if review[0] == '+':
+                all_tokens_in_positives += 1
+            if review[0] == '-':
+                all_tokens_in_negatives += 1
+    differential_score = (math.log(1 + review_type_appearances['positive']) - math.log(1 + all_tokens_in_positives)) - (
+            math.log(1 + review_type_appearances['negative']) - math.log(1 + all_tokens_in_negatives))
+    return differential_score
+
+
+def compute_token_classification(differential_score):
+    if differential_score < -0.1:
+        return 'negative'
+    elif differential_score > 0.1:
+        return 'positive'
+    else:
+        return 'neutral'
+
+
+def show_token_statistics(reviews, tokens):
+    input_token = input("Enter a token: ").lower()
+    if input_token in tokens:
+        review_type_appearances = get_review_type_appearances(input_token, reviews)
+        differential_score = compute_differential_score(review_type_appearances, reviews)
+        token_classification = compute_token_classification(differential_score)
+        print(
+            f'The token "{input_token}" has {review_type_appearances["negative"]} negative, {review_type_appearances["neutral"]} '
+            f'neutral, and {review_type_appearances["positive"]} positive appearance(s) in the training data.')
+        print(
+            f'The token "{input_token}" is classified as {token_classification} because it has has a differential tf-idf '
+            f'score of {differential_score}')
+    else:
+        print(f'The token "{input_token}" does not appear in the training data.')
+
+
+def show_sentence_statistics(tokens, reviews):
+    input_tokens = input('Enter a sentence as space-separated tokens: ').lower().split()
+    token_types = {'positive': 0, 'negative': 0, 'neutral': 0, 'unknown': 0}
+    average_differential_score = 0
+    for input_token in input_tokens:
+        if input_token in tokens:
+            review_type_appearances = get_review_type_appearances(input_token, reviews)
+            differential_score = compute_differential_score(review_type_appearances, reviews)
+            token_types[compute_token_classification(differential_score)] += 1
+            average_differential_score += differential_score
+        else:
+            token_types['unknown'] += 1
+    if len(input_tokens) != token_types['unknown']:
+        average_differential_score /= (len(input_tokens) - token_types['unknown'])
+        print(
+            f'The sentence has {token_types["negative"]} negative, {token_types["neutral"]} neutral, {token_types["positive"]} '
+            f'positive, and {token_types["unknown"]} unknown token(s).')
+        print(f'The sentence has an average tf-idf score of {average_differential_score}.')
+    else:
+        print('The sentence contains only unknown tokens; therefore, its average tf-idf score is undefined.')
+
+
 def main():
     try:
         reviews = make_review_list()
     except FileNotFoundError:
         print("This file does not exist")
         return
-    tokens = make_token_set(reviews)
+    tokens = make_token_dictionary(reviews)
     options = tuple(MenuOption)
     while True:
         print('Choose an option:')
@@ -117,26 +168,15 @@ def main():
         if input_option == MenuOption.EXIT:
             return
         elif input_option == MenuOption.SHOW_REVIEWS:
-            beginning_review_number = get_input_number(f'Enter a beginning review number from 1 to {len(reviews)}: ', 0,
-                                                       reviews)
-            ending_review_number = get_input_number(
-                f'Enter a ending review number from {beginning_review_number} to {len(reviews)}: ',
-                beginning_review_number - 1, reviews)
-            for review in reviews[beginning_review_number - 1:ending_review_number]:
-                print(f'Review #{reviews.index(review) + 1}: {review}')
+            show_reviews(reviews)
         elif input_option == MenuOption.CHECK_TOKEN:
-            input_token = input("Enter a token: ").lower()
-            if input_token in tokens:
-                print(f'The token "{input_token}" is one of the 16444 unique tokens that appear in the training data.')
-            else:
-                print(
-                    f'The token "{input_token}" is not one of the 16444 unique tokens that appear in the training data.')
+            check_token(tokens)
         elif input_option == MenuOption.SHOW_DOCUMENT_FREQUENCY:
-            input_token = input("Enter a token: ").lower()
-            number_of_appearances = get_token_frequency(reviews, input_token)
-            print(f'The training data contains {number_of_appearances} appearance(s) of the token "unmentioned".')
+            show_document_frequency(tokens)
         elif input_option == MenuOption.SHOW_TOKEN_STATISTICS:
-            get_token_statistics(reviews, tokens)
+            show_token_statistics(reviews, tokens)
+        elif input_option == MenuOption.SHOW_SENTENCE_STATISTICS:
+            show_sentence_statistics(tokens, reviews)
         else:
             print(f'{input_option}\n')
 
--- test_sentiment.py
+++ test_sentiment.py
@@ -5,33 +5,36 @@ import sentiment
 class TestMakeTokenSet(TestCase):
     def test_tokens_length(self):
         reviews = sentiment.make_review_list()
-        tokens = sentiment.make_token_set(reviews)
+        tokens = sentiment.make_token_dictionary(reviews)
         self.assertEqual(len(tokens), 16444)
 
 
 class TestGetTokenFrequency(TestCase):
     def test_too(self):
         reviews = sentiment.make_review_list()
-        self.assertEqual(sentiment.get_token_frequency(reviews, 'too'), 314)
+        tokens = sentiment.make_token_dictionary(reviews)
+        frequency = sentiment.get_token_frequency(tokens, 'too')
+        self.assertEqual(frequency, 314)
 
     def test_unmentioned(self):
         reviews = sentiment.make_review_list()
-        self.assertEqual(sentiment.get_token_frequency(reviews, 'unmentioned'), 0)
+        tokens = sentiment.make_token_dictionary(reviews)
+        frequency = sentiment.get_token_frequency(tokens, 'unmentioned')
+        self.assertEqual(frequency, 0)
 
-class TestGetTokenStatistics(TestCase):
+
+class TestReviewTypeAppearances(TestCase):
     def test_positive(self):
         reviews = sentiment.make_review_list()
         review_type_appearances = sentiment.get_review_type_appearances('good', reviews)
-        self.assertEquals(review_type_appearances["positive"], 129)
+        self.assertEqual(review_type_appearances["positive"], 129)
 
     def test_negative(self):
         reviews = sentiment.make_review_list()
         review_type_appearances = sentiment.get_review_type_appearances('good', reviews)
-        self.assertEquals(review_type_appearances["negative"], 85)
+        self.assertEqual(review_type_appearances["negative"], 85)
 
     def test_neutral(self):
         reviews = sentiment.make_review_list()
         review_type_appearances = sentiment.get_review_type_appearances('good', reviews)
-        self.assertEquals(review_type_appearances["neutral"], 50)
-
-
+        self.assertEqual(review_type_appearances["neutral"], 50)
