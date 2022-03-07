5beefad55fa07c61c672b947256e13338d10ccef
dives2 <dives2@huskers.unl.edu>
2022-03-07 13:00:46 -0600

    Refactored test_sentiment so that it only computes the reviews, tokens, and stop words only once

--- test_sentiment.py
+++ test_sentiment.py
@@ -3,73 +3,59 @@ import sentiment
 
 
 class TestSentiment(TestCase):
+    reviews = sentiment.make_review_list('test_sentiment.txt')
+    tokens = sentiment.make_token_dictionary(reviews)
+    stop_words = sentiment.get_stop_words(tokens)
+
     def test_tokens_length(self):
-        reviews = sentiment.make_review_list('test_sentiment.txt')
-        tokens = sentiment.make_token_dictionary(reviews)
-        self.assertEqual(len(tokens), 16444)
+        self.assertEqual(len(self.tokens), 16444)
 
     def test_get_token_frequency_too(self):
-        reviews = sentiment.make_review_list('test_sentiment.txt')
-        tokens = sentiment.make_token_dictionary(reviews)
-        frequency = sentiment.get_token_frequency(tokens, 'too')
+        frequency = sentiment.get_token_frequency(self.tokens, 'too')
         self.assertEqual(frequency, 314)
 
     def test_get_token_frequency_unmentioned(self):
-        reviews = sentiment.make_review_list('test_sentiment.txt')
-        tokens = sentiment.make_token_dictionary(reviews)
-        frequency = sentiment.get_token_frequency(tokens, 'unmentioned')
+        frequency = sentiment.get_token_frequency(self.tokens, 'unmentioned')
         self.assertEqual(frequency, 0)
 
     def test_get_review_type_appearances(self):
-        reviews = sentiment.make_review_list('test_sentiment.txt')
-        review_type_appearances = sentiment.get_review_type_appearances('too', reviews)
+        review_type_appearances = sentiment.get_review_type_appearances('too', self.reviews)
         self.assertEqual(review_type_appearances, {'positive': 49, 'negative': 200, 'neutral': 65})
 
     def test_compute_differential_score(self):
-        reviews = sentiment.make_review_list('test_sentiment.txt')
-        review_type_appearances = sentiment.get_review_type_appearances('too', reviews)
-        score = sentiment.compute_differential_score(review_type_appearances, reviews)
+        review_type_appearances = sentiment.get_review_type_appearances('too', self.reviews)
+        score = sentiment.compute_differential_score(review_type_appearances, self.reviews)
         self.assertEqual(score, -1.4990477543373988)
 
     def test_compute_token_classification_negative(self):
-        reviews = sentiment.make_review_list('test_sentiment.txt')
-        review_type_appearances = sentiment.get_review_type_appearances('too', reviews)
-        score = sentiment.compute_differential_score(review_type_appearances, reviews)
+        review_type_appearances = sentiment.get_review_type_appearances('too', self.reviews)
+        score = sentiment.compute_differential_score(review_type_appearances, self.reviews)
         classification = sentiment.compute_token_classification(score)
         self.assertEqual(classification, 'negative')
 
     def test_compute_token_classification_positive(self):
-        reviews = sentiment.make_review_list('test_sentiment.txt')
-        review_type_appearances = sentiment.get_review_type_appearances('good', reviews)
-        score = sentiment.compute_differential_score(review_type_appearances, reviews)
+        review_type_appearances = sentiment.get_review_type_appearances('good', self.reviews)
+        score = sentiment.compute_differential_score(review_type_appearances, self.reviews)
         classification = sentiment.compute_token_classification(score)
         self.assertEqual(classification, 'positive')
 
     def test_compute_token_classification_neutral(self):
-        reviews = sentiment.make_review_list('test_sentiment.txt')
-        review_type_appearances = sentiment.get_review_type_appearances('the', reviews)
-        score = sentiment.compute_differential_score(review_type_appearances, reviews)
+        review_type_appearances = sentiment.get_review_type_appearances('the', self.reviews)
+        score = sentiment.compute_differential_score(review_type_appearances, self.reviews)
         classification = sentiment.compute_token_classification(score)
         self.assertEqual(classification, 'neutral')
 
     def test_save_stop_word_list(self):
-        reviews = sentiment.make_review_list('test_sentiment.txt')
-        tokens = sentiment.make_token_dictionary(reviews)
-        stop_words = sentiment.get_stop_words(tokens)
-        sentiment.save_stop_word_list(stop_words)
+        sentiment.save_stop_word_list(self.stop_words)
         with open('output.txt', 'r') as output_text:
             number_of_lines = 0
             for line in output_text:
                 number_of_lines += 1
         self.assertEqual(number_of_lines, 51)
 
-    def test_average_idf_score_of_sentence_no_stop_words(self):
-        reviews = sentiment.make_review_list('test_sentiment.txt')
-        tokens = sentiment.make_token_dictionary(reviews)
-        stop_words = sentiment.get_stop_words(reviews, tokens)
-        average_score, token_types = sentiment.calculate_average_score_and_token_types({'positive': 0, 'negative': 0, 'neutral': 0, 'unknown': 0, 'stop_words': 0}, tokens, 'absolutely detestable ; would not watch again'
-                                                                                       , reviews, stop_words)
-        self.assertEqual(token_types, {'positive': 1, 'negative': 5, 'neutral': 0, 'unknown': 1, 'stop_words': 1})
-        self.assertEqual(average_score, -0.18812093738509109)
-
-
+    def test_calculate_average_score_and_token_types_ignoring_stop_words(self):
+        average_score, token_types = sentiment.calculate_average_score_and_token_types(
+            {'positive': 0, 'negative': 0, 'neutral': 0, 'unknown': 0, 'stop_words': 0}, self.tokens,
+            'absolutely detestable ; would not watch again'.split(), self.reviews, self.stop_words)
+        self.assertEqual(token_types, {'positive': 1, 'negative': 4, 'neutral': 0, 'unknown': 1, 'stop_words': 1})
+        self.assertEqual(average_score, -0.17687684751966531)
